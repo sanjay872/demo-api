3d0dd03b8370864efc046d4fa5225b357908b46c
sanjay <rockysanjay287@gmail.com>
2022-03-28 00:39:29 +0530

    script change

--- src/main/java/com/api/controller/ApiController.java
+++ src/main/java/com/api/controller/ApiController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api")
 public class ApiController {
 
-	@GetMapping("/home")
+	@GetMapping()
 	public String welcome() {
 		return "hello welcome!!";
 	}
